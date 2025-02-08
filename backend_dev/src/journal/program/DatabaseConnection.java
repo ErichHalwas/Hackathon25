c8e508daf692f6e36eeaecc0ae90aeeb904add7c
anika1679 <anika.khatri@gmail.com>
2025-02-08 14:12:55 -0500

    CREATE TABLE METHOD
    Added a create table method in DatabaseConnection class and removed unnecessary files.

--- backend_dev/src/journal/program/DatabaseConnection.java
+++ backend_dev/src/journal/program/DatabaseConnection.java
@@ -2,6 +2,9 @@ package journal.program;
 
 import java.sql.*;
 import javax.swing.JOptionPane;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class DatabaseConnection {
     private final String databaseName;
@@ -26,6 +29,39 @@ public class DatabaseConnection {
             JOptionPane.showMessageDialog(null, "Database created successfully");
         } catch (SQLException se) {
             JOptionPane.showMessageDialog(null, se.getMessage());
+        }
+    }
+
+    public void createTable() throws SQLException {
+        File fileName = new File("txt_files\\createTable.txt");
+        try {
+            Connection connection = DriverManager.getConnection(url, userName, password);
+            Scanner scanner = new Scanner(fileName);
+            
+            String sql = "USE " + databaseName;
+            Statement statement = connection.createStatement();
+            statement.executeUpdate(sql);
+
+            StringBuilder sqlBuilder = new StringBuilder();
+            while(scanner.hasNextLine()) {
+                String line = scanner.nextLine().trim();
+                if (!line.isEmpty()) {
+                    sqlBuilder.append(line).append(" ");
+                }
+                if (line.endsWith(";")) { 
+                    statement = connection.createStatement();
+                    statement.executeUpdate(sqlBuilder.toString());
+                    sqlBuilder.setLength(0);
+                }
+            }
+            scanner.close();
+            JOptionPane.showMessageDialog(null, "Tables created successfully");
+        } catch (SQLException se) {
+            JOptionPane.showMessageDialog(null, se.getMessage());
+            System.exit(1);
+        } catch (FileNotFoundException fnfe) {
+            JOptionPane.showMessageDialog(null, fnfe.getMessage());
+            System.exit(2);
         }
     }
 }
