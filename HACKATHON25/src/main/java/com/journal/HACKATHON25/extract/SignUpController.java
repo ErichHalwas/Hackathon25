a2d84f6f5e7eb6b7126537d953b95c537dbe2d39
anika1679 <anika.khatri@gmail.com>
2025-02-08 19:37:53 -0500

    Recent Update

--- HACKATHON25/src/main/java/com/journal/HACKATHON25/extract/SignUpController.java
+++ HACKATHON25/src/main/java/com/journal/HACKATHON25/extract/SignUpController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.journal.HACKATHON25.database.DatabaseConnection;
 
 @RestController
 @RequestMapping("/signup")
@@ -14,6 +15,10 @@ public class SignUpController {
     public void getSignUpCredentials(@RequestBody LoginRequest loginRequest) {
         System.out.println("Username: " + loginRequest.getUsername());
         System.out.println("Password: " + loginRequest.getPassword());
+        //DatabaseConnection db = new DatabaseConnection();
+        //db.createDatabase();
+        //db.createTable();
+        //db.insertData(loginRequest);
     }
     
 
